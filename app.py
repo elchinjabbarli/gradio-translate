d4613dfcfd569a5416957018c697de421637d1df
Elchin Jabbarli <82960034+elchinjabbarli@users.noreply.github.com>
2026-04-08 12:11:31 +0200

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from gradio_client import Client
-
-app = FastAPI()
-client = Client("https://ae242f2f1243cb3eef.gradio.live/")
-
-@app.get("/translate")
-def translate(text: str):
-    return {"translation": client.predict(text, api_name="/predict")}
